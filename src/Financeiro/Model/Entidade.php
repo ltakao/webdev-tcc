fea8735e411278ab8d6b3cd01e0a9aa90cc8ec1d
takao <luciano.takao@gmail.com>
2016-02-02 16:29:04 -0200

    - Mapeamento ORM (Usuario).

---
+++ src/Financeiro/Model/Entidade.php
@@ -0,0 +1,80 @@
+<?php 
+
+namespace Financeiro\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/** @ORM\MappedSuperclass */
+abstract class Entidade 
+{
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @var  datetime
+	 */
+	protected $dataCadastro;
+
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @var  datetime
+	 */
+	protected $dataAlteracao;	
+
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var  integer
+	 */
+	protected $idUsuarioCadastro;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 * @var  integer
+	 */
+	protected $idUsuarioAlteracao;
+
+
+	public function __construct()	
+	{
+		$this->setDataAlteracao(date('Y-m-d H:i:s'));
+	}
+
+	public function getDataCadastro()
+	{
+	    return $this->dataCadastro;
+	}
+	
+	public function setDataCadastro($dataCadastro)
+	{
+	    $this->dataCadastro = \DateTime::createFromFormat('Y-m-d H:i:s', $dataCadastro);
+	}
+
+	public function getDataAlteracao()
+	{
+	    return $this->dataAlteracao;
+	}
+	
+	public function setDataAlteracao($dataAlteracao)
+	{
+	    $this->dataAlteracao = \DateTime::createFromFormat('Y-m-d H:i:s', $dataAlteracao);
+	}
+
+	public function getIdUsuarioCadastro()
+	{
+	    return $this->idUsuarioCadastro;
+	}
+	
+	public function setIdUsuarioCadastro($idUsuarioCadastro)
+	{
+	    $this->idUsuarioCadastro = $idUsuarioCadastro;
+	}
+
+	public function getIdUsuarioAlteracao()
+	{
+	    return $this->idUsuarioAlteracao;
+	}
+	
+	public function setIdUsuarioAlteracao($idUsuarioAlteracao)
+	{
+	    $this->idUsuarioAlteracao = $idUsuarioAlteracao;
+	}
+
+}
---
+++ src/Financeiro/Model/Usuario.php
@@ -0,0 +1,100 @@
+<?php 
+
+namespace Financeiro\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Usuario")
+ */
+class Usuario extends Entidade
+{
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue
+	 * 
+	 * @var integer
+	 */
+	protected $idUsuario = null;	
+
+	/**
+	 * @ORM\Column(type="string", nullable=false)
+	 *
+	 * @var  string
+	 */
+	protected $nome;
+
+	/**
+	 * @ORM\Column(type="string", nullable=false)
+	 *
+	 * @var string
+	 */
+	protected $login;
+
+	/**
+	 * @ORM\Column(type="string", nullable=false)
+	 *
+	 * @var string
+	 */
+	protected $senha;
+
+	/**
+	 * @ORM\Column(type="integer")
+	 *
+	 * @var integer
+	 */
+	protected $administrador;
+
+	
+	public function __construct()
+	{
+		$this->dataCadastro(date("Y-m-d H:i:s"));
+	}
+
+	public function getIdUsuario()
+	{
+		return $this->idUsuario;
+	}
+
+	public function getNome()
+	{
+	    return $this->nome;
+	}
+	
+	public function setNome($nome)
+	{
+	    $this->nome = $nome;
+	}
+
+	public function getLogin()
+	{
+	    return $this->login;
+	}
+	
+	public function setLogin($login)
+	{
+	    $this->login = $login;
+	}
+
+	public function getSenha()
+	{
+		return $this->senha;
+	}
+				
+	public function setSenha($senha)
+	{
+		$this->senha = $senha;
+	}	
+
+	public function getAdministrador()
+	{
+		return $this->administrador;
+	}
+				
+	public function setAdministrador($administrador)
+	{
+		$this->administrador = $administrador;
+	}
+}
